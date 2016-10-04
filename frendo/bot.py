4ad9cba9281d5ab2d4dcf43d2beb0386605eb8be
samhal <halali@student.chalmers.se>
2016-10-04 14:34:11 +0200

    Made some smaller changes!

--- frendo/bot.py
+++ frendo/bot.py
@@ -1,34 +1,36 @@
 import logging
 import datetime
 
-Class Bot:
-    def __init__(self, bot_username, oath_key, msg_template="{}"):
+
+class Bot:
+    def __init__(self, bot_username, oath_key, channel, msg_template="{}"):
         self.s = socket.socket()
         self.bot_username = bot_username
         self.oath_key = oath_key
+        self.channel = channel
         self.msg_template = msg_template
 
-    def join(self, channel):
+    def join(self):
         servername = "irc.twitch.tv"
         port = 6667
-        self.connect(servername,port)
-        self.send_join_msgs(channel)
-        logging.info("[{}]\tSuccessfully connected to the channel!"\
+        self.connect(servername, port)
+        self.send_join_msgs(self.channel)
+        logging.info("[{}]\tSuccessfully connected to the channel!"
                 .format(datetime.datetime.now())
-        self.msg_template = "PRIVMSG #{} :"\
-                "{}\r\n".format(,channel,self.msg_template)
+        self.msg_template = "PRIVMSG #{} :{}\r\n"
+                .format(self.channel,self.msg_template)
         self.say_hello()
 
-    def send_join_msgs(self,channel):
+    def send_join_msgs(self):
         self.send("PASS {}\r\n".format(self.oath_key).encode("utf-8"))
-		self.send("USER {}\r\n".format(self.bot_username).encode("utf-8"))
-		self.send("NICK {}\r\n".format(self.bot_username).encode("utf-8"))
-        self.send("JOIN #{}\r\n".format(channel.lower()).encode("utf-8"))
+        self.send("USER {}\r\n".format(self.bot_username).encode("utf-8"))
+        self.send("NICK {}\r\n".format(self.bot_username).encode("utf-8"))
+        self.send("JOIN #{}\r\n".format(self.channel.lower()).encode("utf-8"))
 
     def say_hello(self):
         self.send_bot_msg(
-                "Hello! I am a bot built with Frendo. You "\
-                "can always build me a friend."\
+                "Hello! I am a bot built with Frendo. You "
+                "can always build me a friend."
                 " Check it out @ https://github.com/samhal/frendo !")
 
     def send(self, msg):
--- frendo/main.py
+++ frendo/main.py
@@ -1,8 +1,24 @@
 import configparser
 import logging
+import os
+
 
 def main():
+    if os.path.isfile("~/.frendorc"):
+        bot = get_bot_from_config()
+    else:
+        bot = get_bot_from_user()
+    bot.join()
+
+
+def get_bot_from_user():
     config = configparser.Configparser()
-    config.read("bot.ini")
+    print("Please read the instructions on how to answer following question"
+          "@ https://github.com/samhal/frendo\n")
+    bot_username = input("Bot username: ")
+    oath_key = input("Oath-key: ")
+    channel = input("Channel to join (e.g Lirik): ")
+    return Bot()
+
 if __name__ == "__main__":
     main()
