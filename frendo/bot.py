24fb128bb8e3e16cb052e721f90993974decc4c7
samhal <halali@student.chalmers.se>
2016-10-06 10:39:14 +0200

    Added frendo class and minor fixes

--- frendo/bot.py
+++ frendo/bot.py
@@ -1,12 +1,13 @@
 import logging
 import datetime
+import socket
 
 
 class Bot:
-    def __init__(self, bot_username, oath_key, channel, msg_template="{}"):
+    def __init__(self, bot_username, oauth_token, channel, msg_template="{}"):
         self.s = socket.socket()
         self.bot_username = bot_username
-        self.oath_key = oath_key
+        self.oauth_token = oauth_token
         self.channel = channel
         self.msg_template = msg_template
 
@@ -14,18 +15,20 @@ class Bot:
         servername = "irc.twitch.tv"
         port = 6667
         self.connect(servername, port)
-        self.send_join_msgs(self.channel)
-        logging.info("[{}]\tSuccessfully connected to the channel!"
-                .format(datetime.datetime.now())
-        self.msg_template = "PRIVMSG #{} :{}\r\n"
-                .format(self.channel,self.msg_template)
+        self.send_join_msgs()
+        (
+            logging.info("[{}]\tSuccessfully connected to the channel!"
+                .format(datetime.datetime.now()))
+        )
+        self.msg_template = ("PRIVMSG #{} :{}\r\n"
+                .format(self.channel, self.msg_template))
         self.say_hello()
 
     def send_join_msgs(self):
-        self.send("PASS {}\r\n".format(self.oath_key).encode("utf-8"))
-        self.send("USER {}\r\n".format(self.bot_username).encode("utf-8"))
-        self.send("NICK {}\r\n".format(self.bot_username).encode("utf-8"))
-        self.send("JOIN #{}\r\n".format(self.channel.lower()).encode("utf-8"))
+        self.send("PASS {}\r\n".format(self.oauth_token))
+        self.send("USER {}\r\n".format(self.bot_username))
+        self.send("NICK {}\r\n".format(self.bot_username))
+        self.send("JOIN #{}\r\n".format(self.channel.lower()))
 
     def say_hello(self):
         self.send_bot_msg(
@@ -34,11 +37,11 @@ class Bot:
                 " Check it out @ https://github.com/samhal/frendo !")
 
     def send(self, msg):
-        self.s.send(msg)
+        self.s.send(msg.encode("utf-8"))
 
     def connect(self, servername, port):
-        self.s.connect(servername,port)
+        self.s.connect((servername, port))
 
-    def send_bot_msg(self,msg):
+    def send_bot_msg(self, msg):
         bot_msg = self.msg_template.format(msg)
         self.send(bot_msg)
--- frendo/main.py
+++ frendo/main.py
@@ -1,24 +1,63 @@
-import configparser
-import logging
+#! /usr/bin/python3
+from configparser import ConfigParser
 import os
+from bot import Bot
+from _thread import start_new_thread
 
 
-def main():
-    if os.path.isfile("~/.frendorc"):
-        bot = get_bot_from_config()
-    else:
-        bot = get_bot_from_user()
-    bot.join()
+class Frendo:
+    def __init__(self):
+        self.configfile_path = os.path.expanduser("~/.frendorc")
 
+    def start(self):
+        if os.path.isfile(self.configfile_path):
+            bot = self.get_bot_from_config()
+        else:
+            bot = self.get_bot_from_user()
+        bot.join()
 
-def get_bot_from_user():
-    config = configparser.Configparser()
-    print("Please read the instructions on how to answer following question"
-          "@ https://github.com/samhal/frendo\n")
-    bot_username = input("Bot username: ")
-    oath_key = input("Oath-key: ")
-    channel = input("Channel to join (e.g Lirik): ")
-    return Bot()
+    def get_bot_from_user(self):
+        print("Please read the instructions on how to answer following question"
+              "@ https://github.com/samhal/frendo\n")
+        bot_username = self.get_bot_username_from_user()
+        oauth_token = self.get_oauth_token_from_user()
+        channel = self.get_channel_from_user()
+        msg_template = self.get_msg_template_from_user()
+        start_new_thread(self.write_configfile, (channel,
+                bot_username, oauth_token, msg_template,))
+        return Bot(bot_username, oauth_token, channel, msg_template)
+
+    def get_bot_from_config(self):
+        config = ConfigParser()
+        config.read(self.configfile_path)
+        bot_section = config["BOT"]
+        channel_section = config["CHANNEL"]
+        bot_username = bot_section["bot_username"]
+        oauth_token = bot_section["oauth_token"]
+        msg_template = bot_section["message_template"]
+        channel = channel_section["channel_name"]
+        return Bot(bot_username, oauth_token, channel, msg_template)
+
+    def write_configfile(self, channel, bot_username, oauth_token, msg_template):
+        config = ConfigParser()
+        config["CHANNEL"] = {"channel_name": channel}
+        config["BOT"] = {"bot_username": bot_username, "oauth_token": oauth_token,
+                "message_template": msg_template}
+        with open(self.configfile_path, "w+") as configfile:
+            config.write(configfile)
+
+    def get_oauth_token_from_user(self):
+        return input("oauth-key: ")
+
+    def get_bot_username_from_user(self):
+        return input("Bot username: ")
+
+    def get_channel_from_user(self):
+        return input("Channel to join (e.g Lirik): ")
+
+    def get_msg_template_from_user(self):
+        return input("Template for messages (e.g Kappa \{\} Kappa): ")
 
 if __name__ == "__main__":
-    main()
+    frendo = Frendo()
+    frendo.start()
